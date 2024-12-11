56d6d4e6393242968f6a0277fc08be2f6737fe14
tope-olajide <31278728+tope-olajide@users.noreply.github.com>
2024-12-11 10:07:02 +0100

    feat: implement main menu with assets and styling
    - Created main menu components with game title and play button
    - Designed layout with multiple font styles for visual appeal
    - Added new image assets for enhanced UI
    - Styled main menu for responsiveness and user experience
    - Removed unnecessary files to streamline the project structure

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,29 @@
 import { Stack } from "expo-router";
-
+import { useFonts } from 'expo-font';
+import { useEffect } from "react";
+import * as SplashScreen from 'expo-splash-screen';
 export default function RootLayout() {
-  return <Stack />;
+  const [loaded] = useFonts({
+    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
+    Assassin: require('../assets/fonts/Assassin.ttf'),
+    CANDSB: require('../assets/fonts/CANDSB.ttf'),
+    HungryBeast: require('../assets/fonts/HungryBeast.ttf'),
+    JungleAdventurer: require('../assets/fonts/JungleAdventurer.ttf'),
+    Snowcandycandy: require('../assets/fonts/Snowcandycandy.otf'),
+    Bastinsondemo: require('../assets/fonts/Bastinsondemo-MV57n.otf'),
+  });
+  useEffect(() => {
+    if (loaded) {
+      SplashScreen.hideAsync();
+    }
+  }, [loaded]);
+
+  if (!loaded) {
+    return null;
+  }
+  return (
+    <Stack>
+ <Stack.Screen options={{ headerShown: false }} name="index" />
+    </Stack>
+  );
 }
