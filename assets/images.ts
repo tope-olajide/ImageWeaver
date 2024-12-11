5d2e06c279443d5d68ef0e1096ece4d98257ac14
tope-olajide <31278728+tope-olajide@users.noreply.github.com>
2024-12-11 09:30:17 +0100

    Reset project to get a fresh app directory

---
+++ assets/images.ts
@@ -0,0 +1,15 @@
+const images = {
+    bg1:require("./images/bg/bg-1.jpg"),
+    bg2:require("./images/bg/bg-2.jpg"),
+    bg3:require("./images/bg/bg-3.jpg"),
+    bg4:require("./images/bg/bg-4.jpg"),
+    bg5:require("./images/bg/bg-5.jpg"),
+    bg6: require("./images/bg/bg-6.jpg"),
+    button1: require("./images/button-1.png"),
+    button2: require("./images/button-2.png"),
+    button3: require("./images/button-3.png"),
+    button4: require("./images/button-4.png"),
+    button5: require("./images/button-5.png"),
+    sample: require("./images/sample.webp"),
+}
+export default images
